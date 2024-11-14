6b75070f3d8a54df21860a8959bfa728a427ee57
D. Artur <daniel.arturjsb@gmail.com>
2024-11-14 17:32:59 -0300

    alterations

---
+++ public/plansEnterprise.ts
@@ -0,0 +1,38 @@
+export const plansEnterprise = [
+  {
+    title: "500 Mega",
+    features: [
+      "100% fibra óptica",
+      "Instalação grátis",
+      "Wi-fi de alta performance",
+      "Assistência premium 24 horas",
+      "Upload 250Mpbs",
+    ],
+    price: "180,00",
+    link: "https://wa.me/558584848836?text=Ol%C3%A1%2C+Gostaria+de+mais+informa%C3%A7%C3%B5es+sobre+o+plano+corporativo+de+500MB.",
+  },
+  {
+    title: "700 Mega",
+    features: [
+      "100% fibra óptica",
+      "Instalação grátis",
+      "Wi-fi de alta performance",
+      "Assistência premium 24 horas",
+      "Upload 250Mpbs",
+    ],
+    price: "299,99",
+    link: "https://wa.me/558584848836?text=Ol%C3%A1%2C+Gostaria+de+mais+informa%C3%A7%C3%B5es+sobre+o+plano+corporativo+de+700MB.",
+  },
+  {
+    title: "1 Giga",
+    features: [
+      "Instalação grátis",
+      "100% fibra óptica",
+      "Wi-fi de alta performance",
+      "Assistência premium 24 horas",
+      "Upload 500Mpbs",
+    ],
+    price: "499,99",
+    link: "https://wa.me/558584848836?text=Ol%C3%A1%2C+Gostaria+de+mais+informa%C3%A7%C3%B5es+sobre+o+plano+corporativo+de+1gb.",
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,6 @@
 'use client'
-import { useEffect, useRef, useState } from "react"
-import nodemailer from "nodemailer"
-import Link from "next/link"
-import ServicePage from "./pages/servicePage"
-import SuportPage from "./pages/suportPage"
-import PricingPage from "./pages/pricingPage"
-import ContactFormPage from "./pages/contactFormPage"
-import HomePage from "./pages/homePage"
 import Footer from "./components/footer"
 import Navbar from "./components/Navbar"
-import ServicesPage from "./pages/servicePage"
 import PlansPage from "./pages/plansPage"
 
 
